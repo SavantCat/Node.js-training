453e13cf15d790089b929862d003fc9e0695ae9e
SavantCat <scanf@me.com>
2014-07-09 19:32:10 +0900

    update

---
+++ unity/websocket/Assets/move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float offset = 1;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (Mathf.Sin (Time.deltaTime* offset) * 1, 0, 0);
+	}
+}
